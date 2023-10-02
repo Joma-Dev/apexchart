f77aa7459e14b591b58aabce736904722c0adbfa
joma-Dev <shakibul.islam@jomma.online>
2023-10-02 17:39:38 +0600

    running

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import ApexChart from '../components/ApexChart';
+
+const Home = () => {
+  return (
+    <div >
+      <h1>Next.js ApexCharts Example</h1>
+      <ApexChart />
+    </div>
+  );
+};
+
+export default Home;
